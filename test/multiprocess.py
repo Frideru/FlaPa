c6806e5f3b587f4e5b1d3da9e8349537c0e8a816
Frideru <zykinf@gmail.com>
2024-11-05 17:03:30 +0500

    add multiprocess test

---
+++ test/multiprocess.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import time
+
+def myFunction():
+    print("Same process")
+    time.sleep(3)
+
+def twoFunc():
+    print("two")
+
+if __name__ == "__main__":
+    p = multiprocessing.Process(target=myFunction)
+    t = multiprocessing.Process(target=twoFunc)
+    p.start() # Запускает процесс
+    p.join() # Ожидает завершение процесса
+    t.start()
+    t.join()
