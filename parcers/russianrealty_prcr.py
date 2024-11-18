abff80bc05ab591a446d83a0b52ace69bf89c6f0
Frideru <zykinf@gmail.com>
2024-11-18 22:48:36 +0500

    russian realty work with actual orders

--- parcers/russianrealty_prcr.py
+++ parcers/russianrealty_prcr.py
@@ -3,9 +3,9 @@ from   bs4 import BeautifulSoup
 import csv
 import re
 
-#main_link = 'https://ekaterinburg.russianrealty.ru/prodazha-kvartiry-452108487-1-komnatnaya-Ekaterinburg-ulitsa-Stepana-Razina-Chkalovskaya/'
 #main_link = 'https://ekaterinburg.russianrealty.ru/prodazha-kvartiry-442460908-3-komnatnaya-Ekaterinburg-ulitsa-Stepana-Razina-Chkalovskaya/'
-main_link = 'https://www.russianrealty.ru/prodazha-kvartiry-260787057-4-komnatnaya-Moskva-pereulok-Banny-Prospekt-mira/'
+#main_link = 'https://www.russianrealty.ru/prodazha-kvartiry-260787057-4-komnatnaya-Moskva-pereulok-Banny-Prospekt-mira/'
+main_link = 'https://ekaterinburg.russianrealty.ru/prodazha-kvartiry-459364523-2-komnatnaya-Ekaterinburg-ulitsa-Amundsena-Chkalovskaya/'
 def Russianrealty(link):
     r                   = requests.get(link)
     soup                = BeautifulSoup(r.text, 'html5lib')
@@ -16,6 +16,7 @@ def Russianrealty(link):
     find_floors         = find_floors.find("li")
     find_address        = soup.find_all("span", class_="street-address")
     find_contacts       = soup.find_all("div", class_="list-contact vcard")
+    find_phone          = soup.find_all("script")
     
     # Комнаты
     rooms = re.findall(r'\s\d\-', find_rooms[0].get_text())[0].replace(' ', '')
@@ -40,8 +41,19 @@ def Russianrealty(link):
 
     # Контакты
     name = soup.find("strong", class_="fn agent")
-    name = name.get_text()
+    if name == None:
+        name = soup.find("strong", class_="fn")
+        name = name.get_text()
+    else:
+        name = name.get_text()
     #phone =
+
+    for script in find_phone:
+        if script.string:
+            script_content = script.string
+            if 'ajax' in script_content:
+                phone = re.findall(r'tel:(\d{11})|(\d{1} \d{3} \d{3}-\d{2}-\d{2})', script_content)
+                phone = f'{phone[0][0]} {phone[2][0] if 2 < len(phone) else ""}'
 
     full_info_json = {
         "rooms":               rooms,
@@ -53,49 +65,47 @@ def Russianrealty(link):
         "price":               price,
         "address_street":      street,
         "address_flat_number": number,
-        "phone":               '',
+        "phone":               phone,
         "name":                name,
         "remont":              '0',
         "link":                link
     }
-    print(full_info_json)
-    print("oki test!")
-    # with open('../data.csv', 'a', newline='') as csvfile:
-    #     spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #     # spamwriter.writerow(
-    #     #     ['Улица']     + 
-    #     #     ['№ Дома']    + 
-    #     #     ['Год']       + 
-    #     #     ['Ссылка']    + 
-    #     #     ['Этаж']      + 
-    #     #     ['Этажей']    + 
-    #     #     ['Общая']     + 
-    #     #     ['Кухня']     + 
-    #     #     ['Комната']   + 
-    #     #     ['Потолок']   + 
-    #     #     ['Ремонт']    + 
-    #     #     ['Стоимость'] + 
-    #     #     ['Телефон']   + 
-    #     #     ['Имя']
-    #     # )
-    #     spamwriter.writerow(
-    #         [full_info_json['address_street']]      + 
-    #         [full_info_json['address_flat_number']] + 
-    #         ["0"]                                   + 
-    #         [full_info_json['link']]                + 
-    #         [full_info_json['floors']]              + 
-    #         [full_info_json['floor']]               + 
-    #         [full_info_json['full_space']]          + 
-    #         [full_info_json['kitchen_space']]       + 
-    #         [full_info_json['room_space']]          + 
-    #         ["0"]                                   + 
-    #         [full_info_json['remont']]              + 
-    #         [full_info_json['price']]               + 
-    #         [full_info_json['phone']]               + 
-    #         [full_info_json['name']]
-    #     )
+    with open('./data.csv', 'a', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # spamwriter.writerow(
+        #     ['Улица']     + 
+        #     ['№ Дома']    + 
+        #     ['Год']       + 
+        #     ['Ссылка']    + 
+        #     ['Этаж']      + 
+        #     ['Этажей']    + 
+        #     ['Общая']     + 
+        #     ['Кухня']     + 
+        #     ['Комната']   + 
+        #     ['Потолок']   + 
+        #     ['Ремонт']    + 
+        #     ['Стоимость'] + 
+        #     ['Телефон']   + 
+        #     ['Имя']
+        # )
+        spamwriter.writerow(
+            [full_info_json['address_street']]      + 
+            [full_info_json['address_flat_number']] + 
+            ["0"]                                   + 
+            [full_info_json['link']]                + 
+            [full_info_json['floors']]              + 
+            [full_info_json['floor']]               + 
+            [full_info_json['full_space']]          + 
+            [full_info_json['kitchen_space']]       + 
+            [full_info_json['room_space']]          + 
+            ["0"]                                   + 
+            [full_info_json['remont']]              + 
+            [full_info_json['price']]               + 
+            [full_info_json['phone']]               + 
+            [full_info_json['name']]
+        )
 
-        # print("OK!")
-        # return "OK!"
+        print("OK!")
+        return "OK!"
 
-Russianrealty(main_link)
+# Russianrealty(main_link)
