7400b7585f3c2d9c4de4eeec4aa3421aa7547fa4
Frideru <zykinf@gmail.com>
2024-11-21 16:42:49 +0500

    mirkvartir some structured

--- parcers/mirkvartir_prcr.py
+++ parcers/mirkvartir_prcr.py
@@ -2,6 +2,7 @@ import requests
 from   bs4 import BeautifulSoup
 import csv
 import re
+import json
 
 # main_link = "https://www.mirkvartir.ru/312591258/"
 # main_link = "https://www.mirkvartir.ru/313375186/"
@@ -14,8 +15,8 @@ import re
 # main_link = input("enter mirkvartir link ex.(https://www.mirkvartir.ru/313375186/)\n--> ")
 
 #main_link = 'https://www.mirkvartir.ru/319529218/' #Комнаты
-#main_link = 'https://www.mirkvartir.ru/316178961/' #Комната
-main_link = 'https://www.mirkvartir.ru/329549152/' #Много параметров
+main_link = 'https://www.mirkvartir.ru/316178961/' #Комната
+#main_link = 'https://www.mirkvartir.ru/329549152/' #Много параметров
 
 
 flat_number = re.findall(r'\d*\d', main_link)[0]
@@ -37,66 +38,54 @@ header = {
 r_site = requests.get(main_link, headers=header)
 r_api  = requests.get(api_link, headers=header)
 
-raw  = r_api.json()
+api  = r_api.json()
 soup = BeautifulSoup (r_site.text, 'html.parser')
 iff  = soup.find_all("div", class_="sc-kjwnom dOPiKV")
 desc = soup.find_all("div", class_="sc-jqbzwb juivVv")
 
-arr = []
-second_row = soup.find_all("div", class_="noabbr sc-sxcbzv bZiYbk")
-sec_row = soup.find_all("div", class_="sc-sxcbzv bZiYbk")
-arr.append(second_row[0].get_text())
+find_rooms  = soup.find_all("div", class_="noabbr sc-sxcbzv bZiYbk")
+find_params = soup.find_all("div", class_="sc-sxcbzv bZiYbk")
+find_price  = soup.find_all("div", class_="price m-all")
 
-print(second_row)
-print(sec_row)
+price = find_price[0].get_text().strip().replace(r'руб.', '')
+phone = api["contactCardViewModel"]["phones"][0]
 
-for i in sec_row:
-    
-    arr.append(i.get_text())
+print("=== IFF ===")
+for i in iff:
+    print(i.get_text())
+print("=== DESC ===")
+for i in desc:
+    print(i.get_text())
 
-info_array = [1,2,3,4,5,6]
+array = [1,2,3,4,5]
 
-# for i in arr:
-#     print(i)
-#     if(i == "комнаты" || i == "комнатa"):
-#         info_array[0] = second_row[0].get_text()
-#         print(i)
-#     elif(i == "Общая площадь"):
-#         print(i)
-#     elif(i == "площадь кухни"):
-#         print(i)
-#     elif(i == "Этаж"):
-#         print(i)
-#     elif(i == "Стоимость"):
-#         print(i)
-#     elif(i == "Год постройки"):
-#         print(i)
+rooms         = 1
+full_space    = 1
+kitchen_space = 1
+floor         = 1
+floors        = 1
 
-# комнаты
-# Общая площадь
-# площадь кухни
-# этаж
-# Стоимость
+for i in 
 
-if(len(re.findall(r'ул.\s\w*\,\s\d*.\d*|\w*\sул.\,\s\d*.\d*|ул.\s\w*\s\(\w*\.\s\w*\)\,\s\d*.\d*|ул.\s\w*\s\w*\s\w*\,\s\d*.\d*', raw["contactCardViewModel"]["fixedDescriptionBottomLine"])) == 0):
-    addr_S  = raw["contactCardViewModel"]["fixedDescriptionBottomLine"]
-    addr_FN = "undefind"
+if(len(re.findall(r'ул.\s\w*\,\s\d*.\d*|\w*\sул.\,\s\d*.\d*|ул.\s\w*\s\(\w*\.\s\w*\)\,\s\d*.\d*|ул.\s\w*\s\w*\s\w*\,\s\d*.\d*', api["contactCardViewModel"]["fixedDescriptionBottomLine"])) == 0):
+    street_name  = api["contactCardViewModel"]["fixedDescriptionBottomLine"]
+    house_number = "undefind"
 else:
-    addr_F  = re.findall(r'ул.\s\w*\,\s\d*.\d*|\w*\sул.\,\s\d*.\d*|ул.\s\w*\s\(\w*\.\s\w*\)\,\s\d*.\d*|ул.\s\w*\s\w*\s\w*\,\s\d*.\d*', raw["contactCardViewModel"]["fixedDescriptionBottomLine"])[0]
-    addr_S  = re.findall(r'\w{3,}\s\(.\.\s\w{3,}\)|^\w{3,}|[a-zA-Zа-я-А-Я]{3,}|\w{2,}\s\w{4,}\s\w{4,}', addr_F)[0]
-    addr_FN = re.findall(r'\d*.\d*$', addr_F)[0].strip()
+    parsed_address  = re.findall(r'ул.\s\w*\,\s\d*.\d*|\w*\sул.\,\s\d*.\d*|ул.\s\w*\s\(\w*\.\s\w*\)\,\s\d*.\d*|ул.\s\w*\s\w*\s\w*\,\s\d*.\d*', api["contactCardViewModel"]["fixedDescriptionBottomLine"])[0]
+    street_name  = re.findall(r'\w{3,}\s\(.\.\s\w{3,}\)|^\w{3,}|[a-zA-Zа-я-А-Я]{3,}|\w{2,}\s\w{4,}\s\w{4,}', parsed_address)[0]
+    house_number = re.findall(r'\d*.\d*$', parsed_address)[0].strip()
 
 full_info_json = {
-    "rooms"              : iff[0].get_text(),
-    "full_space"         : iff[1].get_text(),
-    "kitchen_space"      : iff[2].get_text(),
-    "floors"             : re.findall(r'\d*\d', iff[3].get_text())[0] if len(iff) > 3 else "0",
-    "floor"              : re.findall(r'\d*\d', iff[3].get_text())[1] if len(iff) > 3 else "0",
-    "price"              : iff[4].get_text() if len(iff) > 4 else "0",
-    "address_street"     : addr_S,
-    "address_flat_number": addr_FN,
-    "phone"              : raw["contactCardViewModel"]["phones"][0],
-    "link"               : 'https://www.mirkvartir.ru/312591258/'
+    "rooms"         : rooms,
+    "full_space"    : full_space,
+    "kitchen_space" : kitchen_space,
+    "floors"        : floors, #re.findall(r'\d*\d', iff[3].get_text())[0] if len(iff) > 3 else "0",
+    "floor"         : floor,  #re.findall(r'\d*\d', iff[3].get_text())[1] if len(iff) > 3 else "0",
+    "price"         : price,
+    "street_name"   : street_name,
+    "house_number"  : house_number,
+    "phone"         : phone,
+    "link"          : main_link
 }
 
 #print(full_info_json)
@@ -120,19 +109,19 @@ full_info_json = {
 #     #     ['Имя']
 #     # )
 #     spamwriter.writerow(
-#         [full_info_json['address_street']]      + 
-#         [full_info_json['address_flat_number']] + 
-#         ["0"]                                   + 
-#         [full_info_json['link']]                + 
-#         [full_info_json['floors']]              + 
-#         [full_info_json['floor']]               + 
-#         [full_info_json['full_space']]          + 
-#         [full_info_json['kitchen_space']]       + 
-#         ["0"]                                   + 
-#         ["0"]                                   + 
-#         ["0"]                                   + 
-#         [full_info_json['price']]               + 
-#         [full_info_json['phone']]               + 
+#         [full_info_json['street_name']]   + 
+#         [full_info_json['house_number']]  +
+#         ["0"]                             + 
+#         [full_info_json['link']]          + 
+#         [full_info_json['floors']]        + 
+#         [full_info_json['floor']]         + 
+#         [full_info_json['full_space']]    + 
+#         [full_info_json['kitchen_space']] + 
+#         ["0"]                             + 
+#         ["0"]                             + 
+#         ["0"]                             + 
+#         [full_info_json['price']]         + 
+#         [full_info_json['phone']]         + 
 #         ["0"]
 #     )
 #
