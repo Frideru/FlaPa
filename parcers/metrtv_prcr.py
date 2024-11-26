edc42d2c6d943ed75e9fe75313653c58f57afce1
Frideru <zykinf@gmail.com>
2024-11-26 13:25:17 +0500

    some upadates

--- parcers/metrtv_prcr.py
+++ parcers/metrtv_prcr.py
@@ -3,7 +3,8 @@ from   bs4 import BeautifulSoup
 import csv
 import re
 
-r                   = requests.get(link)
-soup                = BeautifulSoup(r.text, 'html5lib')
-find_rooms          = soup.find_all("h1", itemprop="name")
+link = "https://www.metrtv.ru/prodaga_i_arenda/sell/flat/693548"
+r          = requests.get(link)
+soup       = BeautifulSoup(r.text, 'html5lib')
+find_rooms = soup.find_all("h1", itemprop="name")
 
